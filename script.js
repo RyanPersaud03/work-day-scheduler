bb9e6e1574ead5511b74f789ca3f3ff38dc44d5a
Ryan Persaud <ryanpersaud811@gmail.com>
2023-09-25 01:06:26 -0400

    added funtion to change colors

--- script.js
+++ script.js
@@ -3,10 +3,7 @@
 // in the html.
 
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
+   // How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     $('.saveBtn').on('click', function () {
@@ -19,7 +16,28 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    currentTime = dayjs().format("HH")
+   // alert(currentTime)
+    //currentTime = "13";
+    console.log(currentTime)
+
+    hours= ["#hour-9","#hour-10","#hour-11","#hour-12","#hour-13","#hour-14","#hour-15","#hour-16","#hour-17"]
+
+    for (let i=0;i<hours.length;i++ ){
+        time = parseInt($(hours[i]).attr("id").substring(5))
+        if (time == currentTime){
+            // set color to present
+           $(hours[i]).addClass("present");
+       } else if (time < currentTime){
+           // set color to future
+           $(hours[i]).addClass("past");
+       } else if (time > currentTime){
+            // set color to past
+           $(hours[i]).addClass("future");
+       }
+    }
+
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
